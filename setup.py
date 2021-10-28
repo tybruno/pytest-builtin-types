eb8afe2285e9d1b4d0675c649587e22fa98be817
tybruno <tybruno@cisco.com>
2021-10-28 12:00:53 -0600

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 """Setup.py file"""
 from setuptools import setup
 
+INSTALL_REQUIRES = ("pytest",)
+
 setup(
     name="pytest-builtin-types",
     version="0.0.1",
+    author="Tybruno",
     # the following makes a plugin available to pytest
-    entry_points={"pytest11": ["builtin-types = pytest_builtin_types"]},
+    entry_points={"pytest11": ["builtin_types = pytest_builtin_types"]},
     # custom PyPI classifier for pytest plugins
     classifiers=["Framework :: Pytest"],
+    python_requires=">= 3.6.0",
+    install_requires=INSTALL_REQUIRES,
 )
