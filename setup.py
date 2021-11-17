cc683d0b3ee7cab0382dee3e1836a300dcd539ae
tybruno <tybruno@cisco.com>
2021-11-17 09:42:14 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 """Setup.py file"""
 from setuptools import setup
 
+__version__ = "v1.1"
+__author__ = "Tyler Bruno"
+
+with open("README.md", "r", encoding="utf-8") as file:
+    README = file.read()
+
 INSTALL_REQUIRES = ("pytest",)
 
 setup(
     name="pytest-builtin-types",
-    version="0.0.1",
-    author="Tybruno",
+    version=__version__,
+    author=__author__,
+    long_description=README,
+    long_description_content_type="text/markdown",
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["builtin_types = pytest_builtin_types"]},
     # custom PyPI classifier for pytest plugins
