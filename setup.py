99e76f764935e0702b0f3b80898cf8dc1c914f36
Tyler Bruno <31492155+tybruno@users.noreply.github.com>
2021-11-17 09:34:15 -0700

    Update setup.py
    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ INSTALL_REQUIRES = ("pytest",)
 
 setup(
     name="pytest-builtin-types",
-    version="0.0.1",
+    version="v1.0",
     author="Tybruno",
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["builtin_types = pytest_builtin_types"]},
